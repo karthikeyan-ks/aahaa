476e81f0045b14817e34d27c956a988c88e7dbe3
SUFIYANJT <jtfinu@gmail.com>
2025-02-13 13:22:26 +0530

    header ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
+import Header from './compounent/header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
 
   return (
-    <div>
-
-    </div>
-  )
-}
-
+    <Router> 
+    <div className="App">
+      <Routes>
+      <Route path="/" element={<Header />} />
+      </Routes>
+      </div>
+    </Router>
+  );
+};
 export default App
