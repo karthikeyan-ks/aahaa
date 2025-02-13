d788ac5fed7078b8031f9e49344cf45e6553cb07
karthikeyan-ks <karthikeyanks3673@gmail.com>
2025-02-13 18:46:58 +0530

    ui change

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import './App.css'
-import Header from './compounent/header';
 import AboutSection from './pages/AboutSection';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
 
   return (
+    <Router> 
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<AboutSection />} />
-        </Routes>
-      </Router>
-    </div>
-
+      <Routes>
+      <Route path="/" element={<AboutSection />} />
+      </Routes>
+      </div>
+    </Router>
   );
 };
 export default App
